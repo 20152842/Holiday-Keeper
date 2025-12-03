c1e4853907d44af006841c4448b185f9b30268fd
yng <namgyu967@gmail.com>
2025-12-03 22:57:24 +0900

    - feat : Country Entity
     - Country Entity : Country 엔티티 작성

--- src/main/java/com/example/holidaykeeper/entity/Country.java
+++ src/main/java/com/example/holidaykeeper/entity/Country.java
@@ -1,4 +1,23 @@
 package com.example.holidaykeeper.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "countries")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Country {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "code", length = 10, unique = true, nullable = false)
+	private String code;
+
+	@Column(name = "name", length = 255, nullable = false)
+	private String name;
 }

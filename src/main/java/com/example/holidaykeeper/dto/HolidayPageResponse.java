2a704d309c885d5b01ac0fc058fba6d9f9354a86
yng <namgyu967@gmail.com>
2025-12-03 23:20:21 +0900

    - feat : HolidayPageResponse
     - HolidayPageResponse : Holiday 페이징 응답 작성

--- src/main/java/com/example/holidaykeeper/dto/HolidayPageResponse.java
+++ src/main/java/com/example/holidaykeeper/dto/HolidayPageResponse.java
@@ -1,4 +1,15 @@
 package com.example.holidaykeeper.dto;
+import lombok.*;
+import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class HolidayPageResponse {
+	private int page;
+	private int size;
+	private long total;
+	private List<HolidayDto> holidayDtosList;
 }

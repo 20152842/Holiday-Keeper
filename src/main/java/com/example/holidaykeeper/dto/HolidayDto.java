3ba25c1410c466348fa9b08841b60274bcbbb605
yng <namgyu967@gmail.com>
2025-12-03 23:19:17 +0900

    - feat : HolidayDto
     - HolidayDto : HolidayDto 작성

--- src/main/java/com/example/holidaykeeper/dto/HolidayDto.java
+++ src/main/java/com/example/holidaykeeper/dto/HolidayDto.java
@@ -1,4 +1,23 @@
 package com.example.holidaykeeper.dto;
 
+import java.time.LocalDate;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class HolidayDto {
+	private String countryCode;
+	private LocalDate date;
+	private String localName;
+	private String name;
+	private Boolean fixed;
+	private Boolean global;
+	private String[] types;
+	private String counties;
+	private Integer launchYear;
+	private java.time.OffsetDateTime createdAt;
 }

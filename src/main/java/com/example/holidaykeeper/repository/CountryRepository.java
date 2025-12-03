d3af7aeb55610ccf6853bb8224d8bd5f668b04d0
yng <namgyu967@gmail.com>
2025-12-03 23:08:22 +0900

    - feat : CountryRepository
     - CountryRepository : CountryRepository 작성

--- src/main/java/com/example/holidaykeeper/repository/CountryRepository.java
+++ src/main/java/com/example/holidaykeeper/repository/CountryRepository.java
@@ -1,4 +1,10 @@
 package com.example.holidaykeeper.repository;
 
-public class CountryRepository {
+import com.example.holidaykeeper.entity.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CountryRepository extends JpaRepository<Country, Long> {
+	Optional<Country> findByCode(String code);
 }

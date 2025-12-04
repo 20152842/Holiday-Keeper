c52c0b55f9ae9921fb5109895bad5d90812c5e3a
yng <namgyu967@gmail.com>
2025-12-04 15:36:00 +0900

    - feat : annotation
     - annotation : configuration 어노테이션 추가

--- src/main/java/com/example/holidaykeeper/config/WebClientConfig.java
+++ src/main/java/com/example/holidaykeeper/config/WebClientConfig.java
@@ -2,8 +2,11 @@ package com.example.holidaykeeper.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+
+@Configuration
 public class WebClientConfig {
 	@Value("${holiday.nager.base-url}")
 	private String nagerBaseUrl;

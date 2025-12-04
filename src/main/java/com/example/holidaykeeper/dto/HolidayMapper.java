1409e5cc970d25145221c6688d35be4edf36b79c
yng <namgyu967@gmail.com>
2025-12-04 18:26:16 +0900

    - feat : HolidayMapper
     - HolidayMapper : holiday 'toentity' 메소드 구현

---
+++ src/main/java/com/example/holidaykeeper/dto/HolidayMapper.java
@@ -0,0 +1,25 @@
+package com.example.holidaykeeper.dto;
+
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Component;
+
+import com.example.holidaykeeper.entity.Holiday;
+import com.example.holidaykeeper.external.dto.NagerHolidayResponse;
+
+@Component
+public class HolidayMapper {
+	public Holiday toEntity(NagerHolidayResponse dto, String countryCode) {
+		return Holiday.builder()
+			.countryCode(countryCode)
+			.date(LocalDate.parse(dto.getDate()))
+			.localName(dto.getLocalName())
+			.name(dto.getName())
+			.fixed(dto.getFixed())
+			.global(dto.getGlobal())
+			.type(String.join(",", dto.getTypes()))
+			.counties(dto.getCounties() != null ? String.join(",", dto.getCounties()) : null)
+			.launchYear(LocalDate.parse(dto.getDate()).getYear())
+			.build();
+	}
+}

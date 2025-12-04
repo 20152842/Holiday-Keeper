f1943aba4507b8ba83c4f46b3039213bed435249
yng <namgyu967@gmail.com>
2025-12-04 15:51:10 +0900

    - feat : response
     - response : external response 클래스 작성

--- src/main/java/com/example/holidaykeeper/external/dto/CountryResponse.java
+++ src/main/java/com/example/holidaykeeper/external/dto/CountryResponse.java
@@ -1,4 +1,13 @@
 package com.example.holidaykeeper.external.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CountryResponse {
+	private String countryCode;
+	private String name;
 }
--- src/main/java/com/example/holidaykeeper/external/dto/NagerHolidayResponse.java
+++ src/main/java/com/example/holidaykeeper/external/dto/NagerHolidayResponse.java
@@ -1,4 +1,20 @@
 package com.example.holidaykeeper.external.dto;
 
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NagerHolidayResponse {
+	private String date;
+	private String localName;
+	private String name;
+	private boolean global;
+	private boolean fixed;
+	private List<String> counties;
+	private List<String> types;
 }

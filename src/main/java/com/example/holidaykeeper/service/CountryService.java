f87c9ccaffe96b26d3b436117b39288a0e3e30d5
yng <namgyu967@gmail.com>
2025-12-04 18:00:58 +0900

    - feat : CountryService
     - CountryService : getallCountrycodes 메소드 작성

--- src/main/java/com/example/holidaykeeper/service/CountryService.java
+++ src/main/java/com/example/holidaykeeper/service/CountryService.java
@@ -1,4 +1,21 @@
 package com.example.holidaykeeper.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.holidaykeeper.entity.Country;
+import com.example.holidaykeeper.repository.CountryRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class CountryService {
+
+	private final CountryRepository countryRepository;
+
+	public List<Country> getAllCountryCodes() {
+		return countryRepository.findAll();
+	}
 }

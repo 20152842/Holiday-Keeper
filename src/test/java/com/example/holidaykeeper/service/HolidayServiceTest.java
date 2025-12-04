68eed3a0e7c5142758f08f89a91d09019ed2b0fc
yng <namgyu967@gmail.com>
2025-12-04 18:54:21 +0900

    - feat : HolidayServiceTest
     - HolidayServiceTest : 기본 구현

--- src/test/java/com/example/holidaykeeper/service/HolidayServiceTest.java
+++ src/test/java/com/example/holidaykeeper/service/HolidayServiceTest.java
@@ -1,4 +1,33 @@
 package com.example.holidaykeeper.service;
 
-public class HolidayServiceTest {
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.holidaykeeper.dto.HolidayMapper;
+import com.example.holidaykeeper.external.service.ExternalNagerClient;
+import com.example.holidaykeeper.repository.CountryRepository;
+import com.example.holidaykeeper.repository.HolidayRepository;
+
+@ExtendWith(MockitoExtension.class)
+class HolidayServiceTest {
+
+	@Mock
+	private ExternalNagerClient holidayClient;
+	@Mock
+	private HolidayRepository holidayRepository;
+	@Mock
+	private CountryRepository countryRepository;
+	@Mock
+	private HolidayMapper holidayMapper;
+
+	@InjectMocks
+	private HolidayService holidayService;
+
+	@Test
+	void refreshHoliday_deletesAndInserts() {
+
+	}
 }

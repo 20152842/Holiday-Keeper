e6de69b6517fa061081d6319becdb8f9439f21c6
yng <namgyu967@gmail.com>
2025-12-06 02:04:50 +0900

    - feat : Listener
     - Listener : job & step Listener 구현

--- src/main/java/com/example/holidaykeeper/batch/job/listener/HolidayStepCompletionListener.java
+++ src/main/java/com/example/holidaykeeper/batch/job/listener/HolidayStepCompletionListener.java
@@ -1,4 +1,28 @@
 package com.example.holidaykeeper.batch.job.listener;
 
-public class HolidayStepCompletionListener {
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class HolidayStepCompletionListener implements StepExecutionListener {
+	@Override
+	public void beforeStep(StepExecution stepExecution) {
+		log.info("Step {} started", stepExecution.getStepName());
+	}
+
+	@Override
+	public ExitStatus afterStep(StepExecution stepExecution) {
+		log.info("Step {} completed: status={}, read={}, write={}, skip={}",
+			stepExecution.getStepName(),
+			stepExecution.getStatus(),
+			stepExecution.getReadCount(),
+			stepExecution.getWriteCount(),
+			stepExecution.getSkipCount());
+		return stepExecution.getExitStatus();
+	}
 }

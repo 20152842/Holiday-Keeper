5da81be887fd52cc5ccbb8813e9e91d52a3c8a3a
yng <namgyu967@gmail.com>
2025-12-06 03:10:11 +0900

    - feat : HolidayRepositoryCustom
     - HolidayRepositoryCustom : 기본 구현

---
+++ src/main/java/com/example/holidaykeeper/repository/HolidayRepositoryCustom.java
@@ -0,0 +1,19 @@
+package com.example.holidaykeeper.repository;
+
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.example.holidaykeeper.entity.Holiday;
+
+public interface HolidayRepositoryCustom {
+	Page<Holiday> search(
+		Integer year,
+		String countryCode,
+		LocalDate from,
+		LocalDate to,
+		String type,
+		Pageable pageable
+	);
+}
